6cfd86d27f25089543983d7e7333c33ccb95528f
alexrnguyen <alexander.nguyen@shaw.ca>
2025-12-06 13:19:54 -0700

    clean up day 2 part 1 solution

--- 2025-12-02/advent-of-code-day-2/advent-of-code-day2.cs
+++ 2025-12-02/advent-of-code-day-2/advent-of-code-day2.cs
@@ -1,56 +1,62 @@
-﻿Console.WriteLine("Enter a file path containing input data: ");
-string filePath = Console.ReadLine();
-string input = File.ReadAllText(filePath);
-
-
-IEnumerable<string> productIDs = input.Split(',');
-long sumOfInvalidIds = 0;
-
-foreach (string productID in productIDs)
+﻿namespace Solution
 {
-    long firstID = long.Parse(productID.Split('-')[0]);
-    long secondID = long.Parse(productID.Split('-')[1]);
-
-    long range = secondID - firstID;
-
-    for (long i = firstID; i <= secondID; i++)
+    public class Solution
     {
-        string idInRange = i.ToString();
-
-        if (idInRange.Length % 2 == 1)
+        public long Part1(IEnumerable<string> productIDs)
         {
-            continue;
+            long sumOfInvalidIds = 0;
+
+            foreach (string productID in productIDs)
+            {
+                long firstID = long.Parse(productID.Split('-')[0]);
+                long secondID = long.Parse(productID.Split('-')[1]);
+
+                long range = secondID - firstID;
+
+                for (long i = firstID; i <= secondID; i++)
+                {
+                    string idInRange = i.ToString();
+
+                    if (idInRange.Length % 2 == 0)
+                    {
+                        int leftPtr = 0;
+                        int rightPtr = idInRange.Length / 2;
+                        string longestSubstring = "";
+                        for (int j = 0; j < idInRange.Length / 2; j++)
+                        {
+                            if (idInRange[leftPtr] == idInRange[rightPtr])
+                            {
+                                longestSubstring += idInRange[j];
+                                leftPtr++;
+                                rightPtr++;
+                            }
+                            else
+                            {
+                                longestSubstring = "";
+                                break;
+                            }
+                        }
+
+                        longestSubstring = string.Concat(Enumerable.Repeat(longestSubstring, 2));
+
+                        if (longestSubstring != "")
+                        {
+                            sumOfInvalidIds += long.Parse(longestSubstring);
+                        }
+                    }
+                }
+
+            }
+            return sumOfInvalidIds;
         }
-        else
+        public static void Main(string[] args)
         {
-            int leftPtr = 0;
-            int rightPtr = idInRange.Length / 2;
-            string longestSubstring = "";
-            for (int j = 0; j < idInRange.Length/2; j++)
-            {
-                if (idInRange[leftPtr] == idInRange[rightPtr])
-                {
-                    longestSubstring += idInRange[j];
-                    leftPtr++;
-                    rightPtr++;
-                }
-                else
-                {
-                    longestSubstring = "";
-                    break;
-                }
-            }
-
-            longestSubstring = string.Concat(Enumerable.Repeat(longestSubstring, 2));
-
-            if (longestSubstring != "")
-            {
-                sumOfInvalidIds += long.Parse(longestSubstring);
-            }
+            Solution solutionInstance = new Solution();
+            Console.WriteLine("Enter a file path containing input data: ");
+            string filePath = Console.ReadLine();
+            string input = File.ReadAllText(filePath);
+            IEnumerable<string> productIDs = input.Split(',');
+            Console.WriteLine($"Part 1 Result: {solutionInstance.Part1(productIDs)}");
         }
     }
-
 }
-
-Console.WriteLine($"Result: {sumOfInvalidIds}");
-return 0;
