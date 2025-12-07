935253d8903d17ba9807bb80b4f493372ab7c95a
alexrnguyen <alexander.nguyen@shaw.ca>
2025-12-06 20:23:08 -0700

    add day 4 part 2 solution

--- 2025-12-04/advent-of-code-2025-day4.js
+++ 2025-12-04/advent-of-code-2025-day4.js
@@ -58,9 +58,42 @@ function part1(content)
         }
     }
 
-    console.log(`Number of accessible rolls: ${numAccessibleRolls}`);
+    console.log(`Part 1 Number of accessible rolls: ${numAccessibleRolls}`);
+}
+
+function part2(content)
+{
+    let totalAccessibleRolls = 0;
+    let numAccessibleRolls = 0;
+    do
+    {
+        numAccessibleRolls = 0;
+        for (let row = 0; row < content.length; row++)
+        {
+            for (let col = 0; col < content[row].length; col++)
+            {
+                if (content[row][col] === '@')
+                {
+                    const neighbours = getAllNeighbors(content, row, col);
+                    const numAdjacentRolls = neighbours
+                        .map(neightbour => neightbour === '@' ? 1 : 0)
+                        .reduce((acc, curr) => acc + curr, 0);
+                    
+                    if (numAdjacentRolls < 4) 
+                    {
+                        content[row][col] = "x";
+                        numAccessibleRolls++;
+                    }
+                }
+            }
+        }
+        totalAccessibleRolls += numAccessibleRolls
+    } while (numAccessibleRolls > 0)
+
+    console.log(`Part 2 Number of accessible rolls: ${totalAccessibleRolls}`);
 }
 
 
 const content = readLinesFromFile("day4-input.txt");
 part1(content);
+part2(content);
